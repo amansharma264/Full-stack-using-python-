34a6ca4e688fcb05e27486f88330625711ae5a1d
amansharma264 <amankumar170789@gmail.com>
2025-11-25 14:31:31 +0530

    update code

--- GUI/basics/models.py
+++ GUI/basics/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class studentDepartment(models.Model):
+    DEPT_NAME=models.CharField(max_length=500)
+    DEPT_DESC=models.CharField(max_length=500)
+    
