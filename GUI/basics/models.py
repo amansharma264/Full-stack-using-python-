ae853db15e888849d7bcbfac30965634aa167fa8
amansharma264 <amankumar170789@gmail.com>
2025-11-26 10:30:25 +0530

    debugging

--- GUI/basics/models.py
+++ GUI/basics/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class StudentDepartment(models.Model):
-    DEPT_NAME=models.CharField(max_length=500)
-    DEPT_DESC=models.CharField(max_length=500)
-    
+    DEPT_NAME = models.CharField(max_length=500)
+    DEPT_DESC = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.DEPT_NAME
